61b4338e682a6aa03abf33a679e3523ad00a28ae
Erziok <63980268+Erziok@users.noreply.github.com>
2020-11-04 19:15:38 -0300

    Añadidas las pruebas unitarias - test.py

--- productos/tests.py
+++ productos/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
-
+import unittest
+from .models import *
+from django.db.models import Q
 # Create your tests here.
+
+class testProducto(unittest.TestCase):
+
+    def test_verif_cod(self):
+        #__gt es oara decir mayor que
+        #__lt es para decir menor que
+        #__gte es oara decir mayor o igual que
+        #__lte es para decir menor o igual que
+        #usar ~Q, = es para decir que sea distinto de (!=)
+        productoBueno = Producto.objects.filter(codigo__gte=1).count()
+        
+        producto = Producto.objects.count()
+        
+        self.assertEqual(productoBueno, producto)
+
+    def test_creacion_producto(self):
+        producto = Producto.objects.create( codigo = 6,
+                                            categoria = 2,
+                                            nombre = 'lechuga',
+                                            precio = 1022,
+                                            imagen = '')
+
+        producto.save()
+        self.assertTrue(producto, True)
+    
+
